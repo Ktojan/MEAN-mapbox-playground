90d633df730e97ba0779c3c18385e6cf6e160476
Andrey <karpovich.andrey@gmail.com>
2024-11-14 21:50:13 +0200

    other files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject, TemplateRef, ViewChild } from '@angular/core';
 import { MapboxGlComponent } from './mapbox-gl/mapbox-gl.component';
 import { NgForOf } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialog, MatDialogModule, MatDialogContent } from '@angular/material/dialog';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NgForOf, MapboxGlComponent],
+  imports: [NgForOf, MapboxGlComponent, MatButtonModule, MatDialogModule, MatDialogContent, MatCardModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {}
+export class AppComponent {
+  @ViewChild('aboutModal') aboutModal: TemplateRef<HTMLElement>;
+  dialog = inject(MatDialog);
+
+  clickAbout(enterAnimationDuration: string, exitAnimationDuration: string) {
+    this.dialog.open(this.aboutModal, { 
+      width: '50vw',
+      enterAnimationDuration,
+      exitAnimationDuration,
+     });
+  }
+
+}
